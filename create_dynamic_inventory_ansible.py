ae1a20254b512d26c1990a875d248b2bdd7243e9
zeroking783 <bakanov.vladimir2016@gmail.com>
2024-12-23 15:13:26 +0300

    fix

--- create_dynamic_inventory_ansible.py
+++ create_dynamic_inventory_ansible.py
@@ -68,12 +68,14 @@ for row in rows:
     logger.debug(f"Добавляю в dynamic inventory новый сервер {inventory_hostname} "
                 f"c ip {row[1]["ansible_host"]} из servers.initial")
     try:
-        base_dynamic_inventory["_meta"]["hostvars"][inventory_hostname] = row[1]
+        server_data = row[1]
+        server_data["id"] = row[0]
+        base_dynamic_inventory["_meta"]["hostvars"][inventory_hostname] = server_data
         base_dynamic_inventory["xray-workers"]["hosts"].append(inventory_hostname)
-        logger.info(f"Новый сервер {inventory_hostname} с ip {row[1]["ansible_host"]} "
+        logger.info(f"Новый сервер {inventory_hostname} с ip {server_data["ansible_host"]} "
                     f"успешно добавлен в dynamic inventory")
     except Exception as e:
-        logger.error(f"Не удалось добавить новый сервер {inventory_hostname} \n{row[1]} в dynamic_inventory")
+        logger.error(f"Не удалось добавить новый сервер {inventory_hostname} \n{server_data} в dynamic_inventory")
 
 base_dynamic_inventory_json = json.dumps(base_dynamic_inventory, indent=4)
 logger.info(f"Динамическое инвентарное хранилище: \n{base_dynamic_inventory_json}")
