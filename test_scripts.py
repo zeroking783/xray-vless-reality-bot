b3aed7ff9af244332a14dca1a2fc63298741b393
zeroking783 <bakanov.vladimir2016@gmail.com>
2024-12-23 15:21:09 +0300

    -

--- test_scripts.py
+++
@@ -1,8 +0,0 @@
-from base_function import delete_something
-from base_function import create_something
-from base_function import create_ssh_keys
-from ssh_client import create_ssh_connection
-from ssh_client import closed_ssh_connection
-import os
-
-create_ssh_keys("server-1")
