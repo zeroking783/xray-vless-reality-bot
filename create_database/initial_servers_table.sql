aa1fa6b9ed422498fe37d02183c605eb59f941ae
zeroking783 <bakanov.vladimir2016@gmail.com>
2024-12-23 15:13:09 +0300

    edit create databases

--- create_database/initial_servers_table.sql
+++ create_database/initial_servers_table.sql
@@ -1,5 +1,14 @@
+CREATE SCHEMA "servers";
+
 CREATE TABLE "servers"."initial" (
-  "id" serial PRIMARY KEY,
+  "id" SERIAL PRIMARY KEY,
   "connect_data" jsonb,
-  "processed" boolean default false
+  "changed" boolean DEFAULT false
 );
+
+CREATE TABLE "servers"."connect" (
+  "id" SERIAL PRIMARY KEY,
+  "actual_data" jsonb,
+  "status" varchar DEFAULT 'initial'
+);
+
