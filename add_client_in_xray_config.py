6a2f019c3429a9f475bff49c57da88f6ebb28b85
zeroking783 <bakanov.vladimir2016@gmail.com>
2025-01-09 20:12:27 +0300

    short ids for every user

--- add_client_in_xray_config.py
+++ add_client_in_xray_config.py
@@ -23,7 +23,7 @@ logger.addHandler(handler)
 
 
 # Just generate uuid
-def create_uuid(email):
+def create_uuid_shotids(email):
     logging.info(f"I enter the command to get the uuid for user {email}")
     command_get_uuid = "/usr/local/bin/xray uuid"
     result_command_get_uuid = subprocess.run(command_get_uuid, shell=True, capture_output=True, text=True)
@@ -41,66 +41,58 @@ def create_uuid(email):
                       f"exit_code: {exit_code_result_command_get_uuid}")
     logging.info("uuid successfully generated")
 
-    return stdout_result_command_get_uuid
+    logging.info(f"I enter the command to get shorts id for user {email}")
+    command_get_uuid = "openssl rand -hex 8"
+    result_command_get_shortids = subprocess.run(command_get_uuid, shell=True, capture_output=True, text=True)
 
+    stdout_result_command_get_shortids = result_command_get_shortids.stdout.strip()
+    stderr_result_command_get_shortids = result_command_get_shortids.stderr.strip()
+    exit_code_result_command_get_shortids = result_command_get_shortids.returncode
 
-def get_old_config_xray():
-    logging.info("Took xray config for processing")
-    xray_config_path = "/usr/local/etc/xray/config.json"
-    try:
-        with open(xray_config_path, "r") as json_file:
-            xray_config_json = json.load(json_file)
-    except PermissionError as e:
-        logging.error(f"Insufficient permissions to read xray configuration file: \n{e}")
-    except Exception as e:
-        logging.error(f"Error while trying to read xray configuration file: \n{e}")
-
-    logging.info("xray config file read successfully")
-    return xray_config_json
-
-
-def save_information_in_file(directory, name_file, information, create_directory=False, sftp_client=None):
-    full_path = pathlib.Path(directory) / name_file
-
-    if create_directory:
-        logging.info(f"Start create directory {directory}")
-        try:
-            full_path.parent.mkdir(parents=True, exist_ok=True)
-        except PermissionError as e:
-            logging.error(f"Insufficient permissions to create directory {directory}: \n{e}")
-        except Exception as e:
-            logging.error(f"Unexpected error creating directory {directory}: \n{e}")
-        logging.info(f"Directory {directory} is present/created")
-
-    if sftp_client is not None:
-        logging.info(f"I start writing to file {full_path} on the remote server")
-        try:
-            with sftp_client.open(str(full_path), 'w') as file:
-                file.write(information)
-        except PermissionError as e:
-            logging.error(f"Insufficient permissions to write to file {full_path}: \n{e}")
-        except Exception as e:
-            logging.error(f"Unexpected error writing to file {full_path}: \n{e}")
-        logging.info(f"{full_path} successfully written")
+    if exit_code_result_command_get_shortids == 0:
+        logging.info("Shorts id received successfully")
+        logging.debug(f"Shorts id: \n{stdout_result_command_get_shortids}")
     else:
-        try:
-            with open(full_path, "w") as file:
-                file.write(information)
-        except PermissionError as e:
-            logging.error(f"Insufficient permissions to write to file {full_path}: \n{e}")
-        except Exception as e:
-            logging.error(f"Unexpected error writing to file {full_path}: \n{e}")
-        logging.info(f"{full_path} successfully written")
+        logging.error(f"Error while getting shorts id: \n"
+                      f"{stderr_result_command_get_shortids}\n"
+                      f"exit_code: {exit_code_result_command_get_shortids}")
+    logging.info("Shorts id successfully generated")
 
 
-def save_new_config_xray(xray_config_new):
+    return stdout_result_command_get_uuid, stdout_result_command_get_shortids
 
-    save_directory = "/usr/local/etc/xray"
-    xray_config_path = pathlib.Path(save_directory) / "config.json"
+
+def get_old_config_inbounds():
+    logging.info("Took xray inbounds config for processing")
+    xray_config_inbounds_path = "/home/xray/confs/inbounds.json"
+    try:
+        with open(xray_config_inbounds_path, "r") as json_file:
+            xray_config_inbounds_json = json.load(json_file)
+    except PermissionError as e:
+        logging.error(f"Insufficient permissions to read xray inbounds config: \n{e}")
+    except Exception as e:
+        logging.error(f"Error while trying to read xray inbounds config: \n{e}")
+
+    logging.info("Xray inbounds config file read successfully")
+    return xray_config_inbounds_json
+
+
+def save_new_inbounds(path, new_inbounds):
+
+    logging.debug(f"Начинаю сохранять inbounds с новым клиентом")
+    try:
+        with open(path, "w") as file:
+            file.write(new_inbounds)
+        logging.info(f"Inbounds с новым клинетом успешно сохранен")
+    except Exception as e:
+        logging.error(f"Ошибка при сохранении inbounds с новым клиентом: \n{e}")
+
+
+def save_new_config_inbounds_xray(path, xray_config_inbounds_new):
 
     try:
-        with open(xray_config_path, "w") as file:
-            file.write(xray_config_new)
+        with open(path, "w") as file:
+            file.write(xray_config_inbounds_new)
     except PermissionError as e:
         logging.error(f"Insufficient permissions to write to file: \n{e}")
     except Exception as e:
@@ -111,23 +103,21 @@ def save_new_config_xray(xray_config_new):
 # Это общая функция для добавления пользователя в конфигурацию xray
 def add_client(email):
 
-    uuid = create_uuid(email)
+    uuid, shortids = create_uuid_shotids(email)
 
     new_client = {
         "id": uuid,
-        "email": email,
         "flow": "xtls-rprx-vision"
     }
 
-    xray_config_old_json = get_old_config_xray()
-    xray_config_new_json = xray_config_old_json
-    xray_config_new_json["inbounds"][0]["settings"]["clients"].append(new_client)
+    xray_config_inbounds_old_json = get_old_config_inbounds()
+    xray_config_inbounds_new_json = xray_config_inbounds_old_json
+    xray_config_inbounds_new_json["inbounds"][0]["settings"]["clients"].append(new_client)
+    xray_config_inbounds_new_json["inbounds"][0]["streamSettings"]["realitySettings"]["shortIds"].append(shortids)
 
-    xray_config_new = json.dumps(xray_config_new_json, sort_keys=True, indent=4)
-    full_path = "/usr/local/etc/xray"
-    name_file = "config.json"
-    save_information_in_file(full_path, name_file, xray_config_new)
-
+    xray_config_inbounds_new = json.dumps(xray_config_inbounds_new_json, sort_keys=True, indent=4)
+    path = "/home/xray/confs/inbounds.json"
+    save_new_config_inbounds_xray(path, xray_config_inbounds_new)
 
 add_client("bakvivas")
 
