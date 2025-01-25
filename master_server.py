cab190c1a1058892abd90fa17bf27ff3be230c8b
zeroking783 <bakanov.vladimir2016@gmail.com>
2025-01-25 23:56:24 +0300

    certificates are being created, you need to add a request to send them

--- master_server.py
+++ master_server.py
@@ -1,4 +1,4 @@
-from fastapi inport FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException
 import grpc
 import xray_pb2
 import xray_pb2_grpc
@@ -8,24 +8,40 @@ import redis
 
 def initialize_resources():
     vault_token = get_vault_token()
-    client = create_vault_client("https://127.0.0.1:8200", vault_token, "/etc/ssl/certs/vault-cert.pem")
+    vault_client = create_vault_client("https://127.0.0.1:8200", vault_token, "/etc/ssl/certs/vault-cert.pem")
 
-    data_db_connect = read_secret_vault(client, 'Cloak/databases/settings_server_db')
+    data_db_connect = read_secret_vault(vault_client, 'Cloak/databases/settings_server_db')
     conn, cursor = connect_to_db(data_db_connect["db_host"], 
                             data_db_connect["db_port"], 
                             data_db_connect["db_name"], 
                             data_db_connect["db_user"], 
                             data_db_connect["user_password"])
 
-    data_db_connect = read_secret_vault(client, 'Cloak/databases/redis')
+    data_db_connect = read_secret_vault(vault_client, 'Cloak/databases/redis')
     r = redis.Redis(host=data_db_connect["db_host"], port=data_db_connect["port"], decode_responses=True)
 
-    return conn, cursor, r
+    return conn, cursor, r, vault_client
 
-def create_tls(server_id):
+
+def create_tls(server_id, vault_client, ip):
+    certificate_response = vault_client.write(
+        f"pki_int/issue/ip-role",
+        common_name="localhost",
+        ip_sans=ip,
+        ttl="72h"
+    )
+    if not certificate_response:
+        sys.exit(f"Не удалось создать сертификат в Vault")
+    crt = certificate_response['data']['certificate']
+    key = certificate_response['data']['private_key']
+
+    with open(f"servers_certificates/{ip}.crt") as f:
+        f.write(crt)
+    with open(f"servers_certificates/{ip}.key") as f:
+        f.write(key)
     
 
-def check_tls(server_id, conn, cursor, r):
+def check_tls(server_id, conn, cursor, r, vault_client, ip):
     server_id = server_id.split('-')[-1]
     redis_responce = r.get(server_id)
     if redis_responce is not None:
@@ -40,20 +56,33 @@ def check_tls(server_id, conn, cursor, r):
             r.set(server_id, '1')
             return True
         else:
-            create_tls(server_id)
+            create_tls(server_id, vault_client, ip)
             return True
 
-def send_request(conn, cursor, r, ip):
-    with grpc.insecure_channel('138.124.31.142:50051') as channel:
+def send_grpc_request(server_id, ip, client_id, conn, cursor, r, vault_client):
+    if not check_tls(server_id, conn, cursor, r, vault_client, ip):
+        sys.exit(f"Не удалось проверить сертификат или не удалось создать его")
+    
+    with open(f"servers_certificates/{ip}.crt", "r") as f:
+        certificate = f.read()
+    with open(f"servers_certificates/{ip}.key", "r") as f:
+        private_key = f.read()
+    
+    credentials = grpc.ssl_channel_credentials(
+        certificate_chain=certificate.encode(),
+        private_key=private_key.encode()
+    )
+
+    with grpc.secure_channel(f"{ip}:50051", credentials) as channel:
         stub = xray_pb2_grpc.XrayClientsServiceStub(channel)  # Используем сгенерированный клиент
         # Отправка запроса AddClient
-        response = stub.AddClient(xray_pb2.ClientIdRequest(name="bakvivas"))
+        response = stub.AddClient(xray_pb2.ClientIdRequest(name=client_id))
         print(f"Received response: UUID={response.uuid}, Short ID={response.shortids}")
 
 @app.on_event("startup")
 async def startup_event():
-    global conn, cursor, r
-    conn, cursor, r = initialize_resources()
+    global conn, cursor, r, vault_client
+    conn, cursor, r, vault_client = initialize_resources()
 
 @app.on_event("shutdown")
 async def shutdown_event():
@@ -62,9 +91,10 @@ async def shutdown_event():
     r.close()
 
 @app.post("/create_client")
-async def create_client(server_id: str, client_id: str):
+async def create_client(server_id: str, client_id: str, ip: str):
+    server_id = server_id.split('-')[-1]
     try:
-        result = await send_grpc_request(server_id, name, conn, cursor, r)
+        result = await send_grpc_request(server_id, ip, client_id, conn, cursor, r, vault_client)
         return result
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
