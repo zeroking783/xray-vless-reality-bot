b08981660a2384b0e72c7f682fd0a32a6a0ff89f
zeroking783 <bakanov.vladimir2016@gmail.com>
2025-01-25 22:49:21 +0300

    add tls cert column

--- create_database/initial_servers_table.sql
+++ create_database/initial_servers_table.sql
@@ -3,7 +3,8 @@ CREATE SCHEMA "servers";
 CREATE TABLE "servers"."initial" (
   "id" SERIAL PRIMARY KEY,
   "connect_data" jsonb,
-  "changed" boolean DEFAULT false
+  "ready" boolean DEFAULT false,
+  "tls_certificate" boolean DEFAULT false
 );
 
 CREATE TABLE "servers"."connect" (
@@ -12,3 +13,12 @@ CREATE TABLE "servers"."connect" (
   "status" varchar DEFAULT 'initial'
 );
 
+CREATE TABLE "servers"."initial_connect" (
+  "initial_id" serial,
+  "connect_id" serial,
+  PRIMARY KEY ("initial_id", "connect_id")
+);
+
+ALTER TABLE "servers"."initial_connect" ADD FOREIGN KEY ("initial_id") REFERENCES "servers"."initial" ("id");
+
+ALTER TABLE "servers"."initial_connect" ADD FOREIGN KEY ("connect_id") REFERENCES "servers"."connect" ("id");
